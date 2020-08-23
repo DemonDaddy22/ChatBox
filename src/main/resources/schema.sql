aafebeaa72088c5c69b970f80161d7f2c2703a5f
DemonDaddy22 <guptarohan369@gmail.com>
2020-08-23 18:10:30 +0530

    add database schema file

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS USERS {
+    userId Integer PRIMARY KEY auto_increment,
+    username VARCHAR,
+    firstName VARCHAR(20),
+    lastname VARCHAR(20),
+    password VARCHAR,
+    salt VARCHAR
+};
+
+CREATE TABLE IF NOT EXISTS MESSAGES {
+    messageId Integer PRIMARY KEY auto_increment,
+    username VARCHAR NOT NULL,
+    messageText VARCHAR NOT NULL
+};
